f3d1a25203a0bdf781dab1fc645eb26e7d49ab06
syntheorem <syntheorem@gmail.com>
2018-07-26 22:16:33 -0500

    Minor adjustments to PreludePrime.Numeric

--- src/PreludePrime/Numeric.hs
+++ src/PreludePrime/Numeric.hs
@@ -1,31 +1,40 @@
+-- | Module which re-exports all numeric types and functions from @base@.
 module PreludePrime.Numeric
-( Int
-, Integer
-, Float
-, Double
-, Word
-, Num(..)
-, Real(..)
-, Integral(..)
-, Fractional(..)
-, Floating(..)
-, RealFrac(..)
-, RealFloat(..)
-, subtract
-, even
-, odd
-, gcd
-, lcm
-, (^)
-, (^^)
-, fromIntegral
-, realToFrac
+(
+-- * Prelude re-exports
+  Prelude.Int
+, Prelude.Integer
+, Prelude.Float
+, Prelude.Double
+, Prelude.Word
+, Prelude.Num(..)
+, Prelude.Real(..)
+, Prelude.Integral(..)
+, Prelude.Fractional(..)
+, Prelude.Floating(..)
+, Prelude.RealFrac(..)
+, Prelude.RealFloat(..)
+, Prelude.subtract
+, Prelude.even
+, Prelude.odd
+, Prelude.gcd
+, Prelude.lcm
+, (Prelude.^)
+, (Prelude.^^)
+, Prelude.fromIntegral
+, Prelude.realToFrac
 
+-- * Other re-exports
+, module Data.Int
+, module Data.Word
 , module Data.Ratio
+, module Numeric
 , module Numeric.Natural
 ) where
 
-
+import Data.Int hiding (Int)
 import Data.Ratio
+import Data.Word hiding (Word)
+import Numeric
 import Numeric.Natural
 import Prelude
